fd6e1ee0ed1a7102e54d594ba6a981dab08ba53e
Debi Prasad Mishra <62232670+debimishra2020@users.noreply.github.com>
2020-08-10 18:42:42 -0700

    c

--- Oracle/Perf-tuning.sql
+++ Oracle/Perf-tuning.sql
@@ -8,28 +8,33 @@ GRANT DBA TO PTUNING;
 SET AUTOTRACE ON EXPLAIN STATISTICS;
 SET AUTOTRACE TRACE ONLY;
 DESCRIBE PLAN_TABLE;
-EXPLAIN PLAN FOR SELECT * FROM ....;
-SELECT * FROM PLAN_TABLE;
-SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY());
+EXPLAIN PLAN FOR SELECT * FROM EMPLOYEES;
+SELECT PLAN_TABLE_OUTPUT FROM TABLE(DBMS_XPLAN.DISPLAY()); OR SELECT * FROM TABLE(DBMS_XPLAN.DISPLAY);
+SELECT * FROM PLAN_TABLE WHERE PLAN_ID = <>;
+
 
                                     
 
-/*Information From Explain Plan*/
+/*Information From Explain Plan Followed by Oracle Optimizer*/
 1. Join Method For the Table
 2. Access Method For the Table
-3. Data Operations(Filter, Sort, ..)
+3. Data Operations(Filter, Sort, Aggregation, Count..)
                                     
 1. Join Method - 
-Nested Loop: There are two tables a driving table and a secondary table.  The rows are accessed from driving table, and are compared againest each records from secondary table.
-Hash Join:  Here Oracle does a full-scan of the driving table, builds a RAM hash table.  The hash join will execute faster than nested loop, this join uses more RAM resources.  
+Nested Loop: Every rows from Inner Table are accessed, and are getting compared with each records from Outer Table.
+             Condition: Select Query having muttiple Tables & Joins, Index shoud have on the Table.
+Hash Join: Oracle does a full-scan of the table, builds a RAM hash table. The hash join will execute faster than nested loop, this join uses more RAM resources.
+             Condition: Select Query having Equality operator & No Index(s), should go for Full Table Access.
 Sort Merge Join: Performes first Sort Operation then Merge.
-
-2. Access Method - Full Table Scan 
-                                    
-                                    
-INSERT Types 
-Conventional Insert 
+             Condition: Select Query having muttiple Tables and Join performed by ">" or "<" Operators not "=". When In-equality operations Optimizer goes for this approach.
+             
+2. Access Method - Full Table Scan, Table Access by ROW-ID, Index Unique Scan, Index Range Scan, Index Skip Scan, Full Index Scan, Fast Full Index Scans, Index Joins,
+   Hash Access, Cluster Acess, Bit Map Index 
+ 
+ 
+INSERT Types Followed by Oracle Optimizer
+1.Conventional Insert 
      Explain Plan For INSERT INTO Employee Values(100,'Smith',78,'23-OCT-1989','California',7890);
-Direct Path Insert 
+2.Direct Path Insert - Fastest
      Explain Plan For INSERT /*+APPEND*/ INTO Employee Values(100,'Smith',78,'23-OCT-1989','California',7890);                            
                              
