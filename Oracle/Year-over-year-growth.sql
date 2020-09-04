10cb8335572f9bb20fda469f8a39dc6e85fb914f
Debi Prasad Mishra <62232670+debimishra2020@users.noreply.github.com>
2020-09-04 16:00:44 -0700

    Year-over-year-growth

--- Oracle/Year-over-year-growth.sql
+++ Oracle/Year-over-year-growth.sql
@@ -147,3 +147,24 @@ LAG(REC_CNT) OVER (ORDER BY CYCLE_DT) AS PREV_CNT,
 ROUND((REC_CNT - (LAG(REC_CNT) OVER (ORDER BY CYCLE_DT))/(LAG(REC_CNT) OVER (ORDER BY CYCLE_DT)))*100,3) || '%' AS PCT_GROWTH                                                                                   
 FROM PRTY_MASTER
 ORDER BY 1 ASC;                                                                                               
+
+/*Cummulative Total Calculation*/   
+CYCLE_DT, QTY
+01/01/2020, 10
+01/02/2020, 12
+01/03/2020, 15  
+02/01/2020, 11
+02/02/2020, 12
+02/03/2020, 13
+                                                          
+/*Output*/
+CYCLE_DT, QTY, CUMM_TOT_QTY
+01/01/2020, 10, 10
+01/02/2020, 12, 22
+01/03/2020, 15, 37  
+02/01/2020, 11, 11
+02/02/2020, 12, 23
+02/03/2020, 13, 36                                                          
+SELECT CYCLE_DT, QTY,SUM(QTY) OVER (PARTITION BY TO_CHAR(CYCLE_DT,'MON') ORDER BY CYCLE_DT ASC) CUMM_TOT_QTY
+FROM PRTY_MASTER  
+ORDER BY CYCLE_DT ASC;                                                                                                                
