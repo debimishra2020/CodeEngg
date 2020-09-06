5a36193c950d5311381d5af014d434794da78491
Debi Prasad Mishra <62232670+debimishra2020@users.noreply.github.com>
2020-09-06 01:36:12 -0700

    cum sum, rolling mean, avg....etc

--- Oracle/rolling-window.sql
+++ Oracle/rolling-window.sql
@@ -1,3 +1,27 @@
+select deptno, ename, sal,
+sum(sal) over (partition by deptno order by sal,ename asc) cum_dept_total,
+sum(sal) over (partition by deptno order by deptno asc) dept_total,
+sum(sal) over (order by deptno,sal asc) cum_total,
+sum(sal) over () total_sal
+from emp order by deptno,sal;
+
+deptno  ename   sal   cum_dept_total    dept_total    cum_total   total_sal
+10      Miller  1300  1300              8750          1300        29025
+10      Clark   2450  3750              8750          3750        29025
+10      King    5000  8750              8750          8750        29025
+20      Smith   800   800               10875         9550        29025      
+20      Adams   1100  1900              10875         10650       29025
+20      Jones   2975  4875              10875         13625       29025
+20      Ford    3000  7875              10875         19625       29025
+20      Scott   3000  10875             10875         19625       29025
+30      James   950   950               9400          20575       29025
+30      Martin  1250  2200              9400          23075       29025
+30      Ward    1250  3450              9400          23075       29025
+30      Turner  1500  4950              9400          24575       29025
+30      Allen   1600  6550              9400          26175       29025
+30      Blake   2850  9400              9400          29025       29025
+
+
 /*Write a query to get cummulative shipped quantity months to date*/   
 ship_day      qty
 01/01/2020    10
