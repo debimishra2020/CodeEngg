4d476d0952f59c331563082bba98535d34c14aa8
Debi Prasad Mishra <62232670+debimishra2020@users.noreply.github.com>
2020-12-01 15:41:14 -0800

    HackerRank SQL Query - Category Easy

--- Oracle - HackerRank/category - easy.sql
+++ Oracle - HackerRank/category - easy.sql
@@ -1,1 +1,24 @@
+Que 1 : Revising the Select Query I
+Link  : https://www.hackerrank.com/challenges/revising-the-select-query/problem
 
+SELECT * FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 100000;
+
+Que 2 : Revising the Select Query II
+Link  : https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+
+SELECT NAME FROM CITY WHERE POPULATION > 120000 AND COUNTRYCODE = 'USA';
+
+Que 3 : Select All
+Link  : https://www.hackerrank.com/challenges/select-all-sql/problem
+
+SELECT * FROM CITY;
+
+Que 4 : Select By ID
+Link  : https://www.hackerrank.com/challenges/select-by-id/problem
+
+SELECT * FROM CITY WHERE ID = 1661;
+
+Que 5 : Japanese Cities' Attributes
+Link  : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
