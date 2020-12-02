c4a6a5d296e807eabb3bea7b285a27cb766605b4
Debi Prasad Mishra <62232670+debimishra2020@users.noreply.github.com>
2020-12-01 16:07:41 -0800

    HackerRank SQL Query - Category Easy

--- Oracle - HackerRank/category - easy.sql
+++ Oracle - HackerRank/category - easy.sql
@@ -22,3 +22,36 @@ Que 5 : Japanese Cities' Attributes
 Link  : https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
 
 SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+Que 6 : Japanese Cities' Names
+Link  : https://www.hackerrank.com/challenges/japanese-cities-name/problem
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
+
+Que 7 : Weather Observation Station 1
+Link  : https://www.hackerrank.com/challenges/weather-observation-station-1/problem
+
+SELECT CITY,STATE FROM STATION;
+
+Que 8 : Weather Observation Station 3
+Link  : https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+SELECT DISTINCT CITY FROM STATION WHERE MOD(ID,2) = 0;
+
+Que 9 : Weather Observation Station 4
+Link  : https://www.hackerrank.com/challenges/weather-observation-station-4/problem
+
+SELECT COUNT(CITY) - COUNT(DISTINCT CITY) FROM STATION;
+
+Que 10 : Weather Observation Station 5
+Link   : https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+select city_min_len from (
+select concat(concat(city,' '),length(city)) as city_min_len,city,length(city) from station 
+where length(city) = (select min(length(city)) from station) 
+order by city asc) where rownum = 1 ;
+
+select city_max_len from (
+select concat(concat(city,' '),length(city)) as city_max_len,city,length(city) from station 
+where length(city) = (select max(length(city)) from station) 
+order by city asc) where rownum = 1 ;
